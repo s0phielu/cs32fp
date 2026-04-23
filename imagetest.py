2cf25ffe9456ce33d8af9365c4c71b5367a4fe46
s0phielu <155197443+s0phielu@users.noreply.github.com>
2026-04-23 16:17:28 +0000

    Thu, Apr 23, 2026, 12:17 PM -04:00

---
+++ imagetest.py
@@ -0,0 +1,6 @@
+import climage
+
+# Convert a local image to a string and print it
+# This uses default settings (80 columns, 256 colors)
+output = climage.convert('path/to/your/image.png')
+print(output)
